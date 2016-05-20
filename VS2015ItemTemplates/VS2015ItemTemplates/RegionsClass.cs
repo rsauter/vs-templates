1739da04f9aca52802ceb8943bb2401ed6d162f2
rsauter <roger.sauter@brevis.ch>
2016-05-20 06:59:46 +0200

    New Simple Class with Regions added

---
+++ VS2015ItemTemplates/VS2015ItemTemplates/RegionsClass.cs
@@ -0,0 +1,36 @@
+﻿namespace VS2015ItemTemplates
+{
+    public class RegionsClass
+    {
+        #region Fields
+
+        #endregion
+
+        public RegionsClass()
+        {
+            
+        }
+
+        #region Properties
+        /* Getter/Setter Properties */
+        
+        /* Calculated Properties */
+
+        #endregion
+
+
+
+        #region Methods
+        /* Public Methods */
+
+        #endregion
+
+
+
+        #region Helpers
+        /* Private Helper-Methods */
+
+        #endregion
+
+    }
+}
